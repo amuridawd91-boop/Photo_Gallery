c2c0c91ed6e99feb57722fe37d60636d448bc825
I AM BATMAN <250103254+amuridawd91-boop@users.noreply.github.com>
2026-05-18 11:01:46 +0000

    modified

--- gallery.js
+++ gallery.js
@@ -1,1 +1,17 @@
+function upDate(preview){
+  console.log(preview)
+  console.log(preview.src)
+  console.log(preview.alt)
+  
+   document.getElementById("image").style.backgroundImage = "url('" + preview.src + "')";
+  document.getElementById("image").innerHTML = preview.alt;
+ 
+ 
+	}
 
+
+
+	function unDo(){
+    document.getElementById("image").innerHTML= "Hover over an image below to display here.";
+		document.getElementById("image").style.backgroundImage = "url()";
+	}
